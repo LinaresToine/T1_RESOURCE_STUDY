9e5837c4ccb7d6012ca318f252726b92c2dd08d4
Antonio <a.linares@cern.ch>
2023-08-04 14:49:46 -0500

    Update T1_env.sh

--- T1_env.sh
+++ T1_env.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 source /afs/cern.ch/user/c/cmst0/env.sh
 BASE_DIR=/afs/cern.ch/work/c/cmst0/private/scripts/T1_Analysis
-rm -rf $BASE_DIR/WMAgent.venv3/
-bash $BASE_DIR/wmagent-venv-deploy.sh -y -i prod -t 2.2.3.1 -d $BASE_DIR/WMAgent.venv3/
-source $BASE_DIR/WMAgent.venv3/bin/activate
+if [ -d $BASE_DIR/WMAgent.venv3/ ]
+then
+    source $BASE_DIR/WMAgent.venv3/bin/activate
+else
+    bash $BASE_DIR/wmagent-venv-deploy.sh -y -i prod -t 2.2.3.1 -d $BASE_DIR/WMAgent.venv3/
+    source $BASE_DIR/WMAgent.venv3/bin/activate
+fi
+
 pip install matplotlib
 pip install numpy
+
+cd $BASE_DIR/
+
