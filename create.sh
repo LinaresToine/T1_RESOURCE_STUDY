5cb50c260971fe89f36640bf44a1f5e10ec1329f
Antonio <a.linares@cern.ch>
2023-08-07 18:48:31 -0500

    update repository with use of flags in file generator

--- create.sh
+++
@@ -1,2 +0,0 @@
-title="$1"
-/afs/cern.ch/work/c/cmst0/private/scripts/T1_Analysis/data_file_generator.sh "" "$title"
--- data_file_generator.sh
+++ data_file_generator.sh
@@ -1,16 +1,26 @@
+#!/bin/bash
 # "This file is responsible for limiting the condor history to a specific replay. The default replay ID is the one for last deployment"
-if [ -z "$1" ]
+
+ID=""
+title=""
+while getopts ":i:t" opt; do 
+    case ${opt} in
+        i) 
+            ID=$OPTARG ;;
+        t) 
+            title=$OPTARG ;;
+    esac
+done 
+
+if [ -z "$ID" ]
 then
     ID="$(cat $install/Tier0Feeder/DeploymentID.txt)"
-else
-    ID="$1"
 fi
 
-if [ -z "$2" ]
+if [ -z "$title" ]
 then 
     title="T1_data"
-else
-    title="$2"
 fi
+
 python3 /afs/cern.ch/work/c/cmst0/private/scripts/T1_Analysis/extract.py "$ID" "$title"
 
