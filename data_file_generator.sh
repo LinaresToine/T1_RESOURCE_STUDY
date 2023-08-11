0787f7c0523c6f8ffa10f7ac772928e9db2e2827
germanfgv <germanfgv@gmail.com>
2023-08-11 19:32:35 +0200

    Automating repository push

--- data_file_generator.sh
+++ data_file_generator.sh
@@ -3,12 +3,15 @@
 
 ID=""
 title=""
-while getopts ":i:t:" opt; do 
+DIRECTORY=/afs/cern.ch/work/c/cmst0/private/scripts/T1_Analysis/T1_RESOURCE_STUDY/
+while getopts ":i:t:d:" opt; do 
     case ${opt} in
         i) 
             ID="$OPTARG" ;;
         t) 
             title="$OPTARG" ;;
+        d)
+            DIR="$DIRECTORY$OPTARG" ;;
     esac
 done 
 
@@ -23,6 +26,9 @@ then
     title="T1_data"
 fi
 
+
+cd $DIR
+
 echo "$title"
 echo "Replay ID is $ID"
 #condor_q | wc -l
@@ -33,4 +39,11 @@ python3 /afs/cern.ch/work/c/cmst0/private/scripts/T1_Analysis/extract.py "$ID" "
 echo "Generating graphs for this T1 site"
 echo " "
 python3 /afs/cern.ch/work/c/cmst0/private/scripts/T1_Analysis/T1_graphs.py "$title"
+
+if [ "$title" != "T1_data" ]
+then
+    git add *"$title"*
+    git commit -m "Data for T1 site $title"
+    git push origin Automate_test_2 
+fi
 
